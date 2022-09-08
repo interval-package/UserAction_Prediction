796b681a7d650db17d492fb520d4e3e3a2249342
Zza <1519162203@qq.com>
2022-09-08 08:59:58 +0800

    add argparse

--- UserAction_main.py
+++ UserAction_main.py
@@ -4,10 +4,20 @@ import argparse
 from UserAction_Run import *
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument()
+parser.add_argument("--verbose", help="increase output verbosity",
+                    action="store_true")
+parser.add_argument("--path", action="path",
+                    default='./runs/logs/model.log',
+                    help="log saving path")
+
 if __name__ == '__main__':
+
+    args = parser.parse_args()
     logging.basicConfig(format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                        level=logging.DEBUG,
-                        filename='./runs/logs/model.log',
+                        level=logging.INFO if args.verbose else logging.WARNING,
+                        filename=args.path,
                         filemode='a')
     obj = UserAction_run()
     obj.train()
